f74cd5dff0c336467f0a6a2b66e021bda7b26d4c
gerardoh13 <gerardoh13@gmail.com>
2023-05-24 23:46:52 -0700

    solved #724

--- script.js
+++ script.js
@@ -129,3 +129,20 @@ function guessNumber(n) {
     else if (currGuess === 1) start = mid + 1;
   }
 }
+
+// 724. Find Pivot Index
+
+function pivotIndex(arr) {
+  let leftSum = 0;
+  let rightSum = 0;
+  for (let i = arr.length - 1; i > 0; i--) {
+    rightSum += arr[i];
+  }
+  for (let j = 0; j < arr.length; j++) {
+    if (leftSum === rightSum) return j;
+    leftSum += arr[j];
+    if (arr[j + 2] === undefined) rightSum = 0;
+    else rightSum -= arr[j + 1];
+  }
+  return -1;
+}
