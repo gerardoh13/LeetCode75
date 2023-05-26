dad65f901078390e68968f1d26abe2714a3f8827
gerardoh13 <gerardoh13@gmail.com>
2023-05-25 23:54:42 -0700

    solved #605

--- script.js
+++ script.js
@@ -146,3 +146,16 @@ function pivotIndex(arr) {
   }
   return -1;
 }
+
+// 605. Can Place Flowers
+
+function canPlaceFlowers(flowerbed, n) {
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i - 1] != 1 && flowerbed[i] === 0 && flowerbed[i + 1] != 1) {
+      flowerbed[i] = 1;
+      n--;
+      if (n <= 0) return true;
+    }
+  }
+  return n <= 0;
+}
