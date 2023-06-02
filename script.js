119509ae508ea10b189b656e4ebe3d7104d0473c
gerardoh13 <gerardoh13@gmail.com>
2023-06-01 23:52:18 -0700

    solved #283

--- script.js
+++ script.js
@@ -229,3 +229,19 @@ function productExceptSelf(nums) {
   }
   return res;
 }
+
+// 283. Move Zeroes
+
+function moveZeroes(nums) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== 0) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  while (i < nums.length) {
+    nums[i] = 0;
+    i++;
+  }
+}
