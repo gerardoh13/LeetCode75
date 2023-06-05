c789b9cd603f405af464269b059e716fbb9d05fc
gerardoh13 <gerardoh13@gmail.com>
2023-06-04 23:53:28 -0700

    solved #933

--- script.js
+++ script.js
@@ -262,3 +262,18 @@ function maxArea(height) {
   }
   return max;
 }
+
+// 933. Number of Recent Calls
+
+var RecentCounter = function() {
+  this.q = []
+};
+
+RecentCounter.prototype.ping = function (t) {
+  this.q.push(t);
+  while (this.q[0] < t - 3000) {
+    this.q.shift();
+  }
+
+  return this.q.length;
+};
