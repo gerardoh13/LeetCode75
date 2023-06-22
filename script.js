e195f72190ca59bfd3d0534f4681b6eee6de147e
gerardoh13 <gerardoh13@gmail.com>
2023-06-21 22:08:11 -0700

    completed #790

--- script.js
+++ script.js
@@ -533,3 +533,21 @@ function equalPairs(grid) {
   }
   return res;
 }
+
+// 790. Domino and Tromino Tiling
+
+function numTilings(n) {
+  let dp = new Array(n).fill(0);
+  dp[0] = 1;
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 5;
+  if (n <= 3) return dp[n];
+  for (let i = 4; i <= n; i++) {
+    dp[i] = 2 * dp[i - 1] + dp[i - 3];
+    dp[i] %= mod;
+  }
+  return dp[n];
+}
+
+console.log(numTilings(4));
