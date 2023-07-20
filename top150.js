24cbcea8532831da4bc873f667ceaf15c769ac38
gerardoh13 <gerardoh13@gmail.com>
2023-07-19 22:40:39 -0700

    solved #125

--- top150.js
+++ top150.js
@@ -22,3 +22,17 @@ function removeDuplicates(nums) {
   }
   return i + 1;
 }
+
+// 125. Valid Palindrome
+
+function isPalindrome(s) {
+  s = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    if (s[i] !== s[j]) return false;
+    i++;
+    j--;
+  }
+  return true;
+}
