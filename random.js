1530fb23e0e6fac664f13610cc935480f4013daf
gerardoh13 <gerardoh13@gmail.com>
2024-04-12 22:46:05 -0700

    solved DC 4/12/24

--- random.js
+++ random.js
@@ -917,3 +917,32 @@ function trap(height) {
   }
   return res;
 }
+
+// 85. Maximal Rectangle
+
+function maximalRectangle() {
+  if (!matrix.length) return 0;
+  const n = matrix[0].length;
+  const heights = new Array(n + 1).fill(0);
+  let maxArea = 0;
+  // debugger;
+  for (let row of matrix) {
+    for (let i = 0; i < n; i++) {
+      heights[i] = row[i] === "1" ? heights[i] + 1 : 0;
+    }
+    const stack = [-1];
+    for (let i = 0; i < n + 1; i++) {
+      while (heights[i] < heights[stack[stack.length - 1]]) {
+        const h = heights[stack.pop()];
+        const w = i - stack[stack.length - 1] - 1;
+        maxArea = Math.max(maxArea, h * w);
+      }
+      stack.push(i);
+    }
+  }
+  return maxArea;
+}
+
+// let matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+
+// console.log(maximalRectangle(matrix))
