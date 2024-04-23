1e02b3590476255d7ecd2cb7afb0142a857b2645
gerardoh13 <gerardoh13@gmail.com>
2024-04-22 22:58:25 -0700

    solved DC 4/22/24

--- random.js
+++ random.js
@@ -1160,7 +1160,7 @@ function openLock(deadends, target) {
   const dead = new Set(deadends);
   const queue = [["0000", 0]];
   const seen = new Set(["0000"]);
-  
+
   for (let [curr, turns] of queue) {
     if (curr === target) return turns;
     if (dead.has(curr)) continue;
@@ -1182,4 +1182,37 @@ function getNextStates(s = "0000") {
   }
 
   return ans;
+}
+
+//
+
+function findMinHeightTrees(n, edges) {
+  if (!edges.length) return [0];
+  const depth = Array(n).fill(0);
+  const graph = Array.from({ length: n }, () => []);
+
+  for (const [parent, child] of edges) {
+    graph[parent].push(child);
+    graph[child].push(parent);
+    depth[child]++;
+    depth[parent]++;
+  }
+
+  const queue = [];
+  let front = 0;
+  for (let i = 0; i < depth.length; i++) {
+    if (depth[i] === 1) queue.push(i);
+  }
+  while (n > 2) {
+    const popEle = queue.length - front;
+    n -= popEle;
+    for (let i = 0; i < popEle; i++) {
+      const currentElm = queue[front++];
+      for (const currentGraph of graph[currentElm]) {
+        depth[currentGraph]--;
+        if (depth[currentGraph] === 1) queue.push(currentGraph);
+      }
+    }
+  }
+  return queue.slice(front);
 }
