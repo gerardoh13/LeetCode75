24f9cdc3258b6cc48479281d6617a2a804826038
gerardoh13 <gerardoh13@gmail.com>
2024-04-24 22:26:49 -0700

    solved DC 4/24/24

--- random.js
+++ random.js
@@ -1216,3 +1216,28 @@ function findMinHeightTrees(n, edges) {
   }
   return queue.slice(front);
 }
+
+// 2370. Longest Ideal Subsequence
+
+function longestIdealString(s, k) {
+  let n = s.length,
+    longest = 0;
+  let maxLength = new Array(n).fill(0);
+  const dfs = (i) => {
+    if (i === n) return 0;
+    if (maxLength[i] > 0) return maxLength[i];
+    let cur = 1,
+      curCode = s.charCodeAt(i);
+    for (let j = i + 1; j < n; j++) {
+      if (Math.abs(s.charCodeAt(j) - curCode) <= k) {
+        cur = Math.max(cur, dfs(j) + 1);
+      }
+      if (s[j] === s[i]) break;
+    }
+    return (maxLength[i] = cur);
+  };
+  for (let i = 0; i < n; i++) {
+    longest = Math.max(longest, dfs(i));
+  }
+  return longest;
+}
