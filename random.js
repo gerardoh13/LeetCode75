b84dd6f88257af5bcccdcb1c61d2aeee8f4ea343
gerardoh13 <gerardoh13@gmail.com>
2024-04-25 23:54:58 -0700

    solved DC 4/25/24

--- random.js
+++ random.js
@@ -1241,3 +1241,25 @@ function longestIdealString(s, k) {
   }
   return longest;
 }
+
+// 1289. Minimum Falling Path Sum II
+
+function minFallingPathSum(grid) {
+  const n = grid.length;
+  const dp = new Array(n).fill().map(() => new Array(n).fill(-1));
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = grid[0][j];
+  }
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let temp = Infinity;
+      for (let k = 0; k < n; k++) {
+        if (j !== k) {
+          temp = Math.min(temp, grid[i][j] + dp[i - 1][k]);
+        }
+      }
+      dp[i][j] = temp;
+    }
+  }
+  return Math.min(...dp[n - 1]);
+}
