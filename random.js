c104fbe00ec3099b4c065235162de190dc66c3b0
gerardoh13 <gerardoh13@gmail.com>
2024-05-10 22:32:12 -0700

    solved DC 5/10/2024

--- random.js
+++ random.js
@@ -1551,3 +1551,30 @@ function kthSmallestPrimeFraction(arr, k) {
   }
   return res;
 }
+
+// 857. Minimum Cost to Hire K Workers
+
+function mincostToHireWorkers(quality, wage, k) {
+  let ratios = [];
+  for (let i = 0; i < quality.length; i++) {
+    ratios.push([wage[i] / quality[i], quality[i], wage[i]]);
+  }
+  ratios.sort((a, b) => a[0] - b[0]);
+  let res = Infinity;
+  let maxHeap = new PriorityQueue({ compare: (a, b) => b - a });
+  let heapSum = 0;
+  for (let i = 0; i < ratios.length; i++) {
+    let ratio = ratios[i][0];
+    let [, qual] = ratios[i];
+    maxHeap.enqueue(qual);
+    heapSum += qual;
+    if (maxHeap.size() > k) {
+      heapSum -= maxHeap.dequeue();
+    }
+    if (maxHeap.size() === k) {
+      res = Math.min(res, heapSum * ratio);
+    }
+  }
+  return res;
+}
+
