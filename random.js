e0ce2aa39c8fc1c1931ca3e46687c36fcb31855b
gerardoh13 <gerardoh13@gmail.com>
2024-05-18 19:52:33 -0700

    Solved DC 5/18/24

--- random.js
+++ random.js
@@ -1733,3 +1733,29 @@ function distributeCoins(root) {
   dfs(root);
   return res;
 }
+
+// 3068. Find the Maximum Sum of Node Values
+
+function maximumValueSum(nums, k, edges) {
+  let totalSum = 0;
+  let count = 0;
+  let positiveMin = Infinity;
+  let negativeMax = -Infinity;
+
+  for (let nodeValue of nums) {
+    let nodeValAfterOperation = nodeValue ^ k;
+    totalSum += nodeValue;
+    let netChange = nodeValAfterOperation - nodeValue;
+
+    if (netChange > 0) {
+      positiveMin = Math.min(positiveMin, netChange);
+      totalSum += netChange;
+      count += 1;
+    } else {
+      negativeMax = Math.max(negativeMax, netChange);
+    }
+  }
+
+  if (count % 2 === 0) return totalSum;
+  return Math.max(totalSum - positiveMin, totalSum + negativeMax);
+}
