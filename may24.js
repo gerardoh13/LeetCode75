b149db28c7e72a279d69336c77787ffe0eee9d4d
gerardoh13 <gerardoh13@gmail.com>
2024-05-29 23:52:33 -0700

    Solved DC 5/29/24

--- may24.js
+++ may24.js
@@ -631,3 +631,22 @@ function numSteps(s) {
   }
   return res;
 }
+
+// 1442. Count Triplets That Can Form Two Arrays of Equal XOR
+
+function countTriplets(arr) {
+  let n = arr.length;
+  let prefix = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; i++) {
+    prefix[i + 1] = prefix[i] ^ arr[i];
+  }
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    for (let k = i + 1; k < n; k++) {
+      if (prefix[i] === prefix[k + 1]) {
+        count += k - i;
+      }
+    }
+  }
+  return count;
+}
