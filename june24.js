cc11db3f01c51dbeee0bb2d5f763ad595720a676
gerardoh13 <gerardoh13@gmail.com>
2024-06-02 23:37:32 -0700

    Solved DC 6/2/24

--- june24.js
+++ june24.js
@@ -19,5 +19,16 @@ function reverseString(s) {
     i++;
     j--;
   }
-  return s;
+}
+
+// 2486. Append Characters to String to Make Subsequence
+
+function appendCharacters(s, t) {
+  let i = 0,
+    j = 0;
+  while (i < s.length) {
+    if (s[i] === t[j]) j++;
+    i++;
+  }
+  return t.length - j;
 }
