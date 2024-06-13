d3a0dc149ced5d21cd39e4dbab50e39c4971fc76
gerardoh13 <gerardoh13@gmail.com>
2024-06-12 20:02:31 -0700

    Solved DC 6/12/24

--- june24.js
+++ june24.js
@@ -214,3 +214,15 @@ function sortColors(nums) {
   }
   return nums;
 }
+
+// 2037. Minimum Number of Moves to Seat Everyone
+
+function minMovesToSeat(seats, students) {
+  seats.sort((a, b) => a - b);
+  students.sort((a, b) => a - b);
+  let res = 0;
+  for (let i = 0; i < seats.length; i++) {
+    res += Math.abs(seats[i] - students[i]);
+  }
+  return res;
+}
