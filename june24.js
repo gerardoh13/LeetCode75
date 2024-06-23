13676a05358c65d8bf76f043a0ff322855320632
gerardoh13 <gerardoh13@gmail.com>
2024-06-22 21:16:42 -0700

    Solved DC 6/22/24

--- june24.js
+++ june24.js
@@ -501,3 +501,27 @@ function numberOfSubarrays(nums, k) {
   return helper(nums, k) - helper(nums, k - 1);
 }
 
+// 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+
+function longestSubarray(nums, limit) {
+  const maxQue = [];
+  const minQue = [];
+  let maxTop = -1,
+    minTop = -1,
+    left = -1;
+  let maxBottom = 0,
+    minBottom = 0;
+  for (let right = 0; right < nums.length; ++right) {
+    const val = nums[right];
+    while (maxTop >= maxBottom && val > maxQue[maxTop]) --maxTop;
+    while (minTop >= minBottom && val < minQue[minTop]) --minTop;
+    maxQue[++maxTop] = val;
+    minQue[++minTop] = val;
+    if (maxQue[maxBottom] - minQue[minBottom] > limit) {
+      ++left;
+      maxQue[maxBottom] === nums[left] && ++maxBottom;
+      minQue[minBottom] === nums[left] && ++minBottom;
+    }
+  }
+  return nums.length - left - 1;
+}
