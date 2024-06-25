f938e4b15b2df697ddcb97250158e9525dbe1ed8
gerardoh13 <gerardoh13@gmail.com>
2024-06-25 16:12:39 -0700

    Solved DC 6/25/24

--- june24.js
+++ june24.js
@@ -561,3 +561,25 @@ function bstToGst(root) {
   }
   return root;
 }
+
+// 1382. Balance a Binary Search Tree
+
+function balanceBST(root) {
+  function inOrder(myRoot) {
+    if (!myRoot) return [];
+    return [...inOrder(myRoot.left), myRoot.val, ...inOrder(myRoot.right)];
+  }
+  const sortedArr = inOrder(root);
+
+  function constructTree(arr) {
+    if (!arr.length) return null;
+
+    const mid = Math.floor(arr.length / 2);
+    const node = new TreeNode(arr[mid]);
+    node.left = constructTree(arr.slice(0, mid));
+    node.right = constructTree(arr.slice(mid + 1));
+
+    return node;
+  }
+  return constructTree(sortedArr);
+}
