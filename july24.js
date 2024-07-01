36639aa308ecdea019511d851c5831b151465481
gerardoh13 <gerardoh13@gmail.com>
2024-06-30 22:44:40 -0700

    Solved DC 6/30/24

---
+++ july24.js
@@ -0,0 +1,13 @@
+// 1550. Three Consecutive Odds
+
+function threeConsecutiveOdds(arr) {
+  if (arr.length < 3) return false;
+  let count = 0;
+  for (let num of arr) {
+    if (num % 2 !== 0) {
+      count++;
+      if (count === 3) return true;
+    } else count = 0;
+  }
+  return false;
+}
