6b19cb674fef3b8c53e002fdcbb357dc9dbf15e3
gerardoh13 <gerardoh13@gmail.com>
2024-07-01 23:06:59 -0700

    Solved DC 7/1/24

--- july24.js
+++ july24.js
@@ -11,3 +11,20 @@ function threeConsecutiveOdds(arr) {
   }
   return false;
 }
+
+// 350. Intersection of Two Arrays II
+
+function intersect(nums1, nums2) {
+  let res = [];
+  let count = new Map();
+  for (let num of nums1) {
+    count.set(num, count.get(num) + 1 || 1);
+  }
+  for (let num of nums2) {
+    if (count.get(num)) {
+      count.set(num, count.get(num) - 1);
+      res.push(num);
+    }
+  }
+  return res;
+}
