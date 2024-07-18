7132a6da64f4253ffc398fc6ba53a0794b0e5d2c
gerardoh13 <gerardoh13@gmail.com>
2024-07-17 22:39:01 -0700

    Solved DC 7/17/24

--- july24.js
+++ july24.js
@@ -418,3 +418,33 @@ function delNodes(root, to_delete) {
   }
   return res;
 }
+
+// 1530. Number of Good Leaf Nodes Pairs
+
+function countPairs(root, distance) {
+  let count = 0;
+  const MAX_DISTANCE = 10;
+
+  function dfs(node) {
+    if (!node) return new Array(MAX_DISTANCE + 1).fill(0);
+    if (!node.left && !node.right) {
+      const res = new Array(MAX_DISTANCE + 1).fill(0);
+      res[1] = 1;
+      return res;
+    }
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+    for (let i = 1; i <= distance; i++) {
+      for (let j = 1; j <= distance - i; j++) {
+        count += left[i] * right[j];
+      }
+    }
+    const res = new Array(MAX_DISTANCE + 1).fill(0);
+    for (let i = 1; i < MAX_DISTANCE; i++) {
+      res[i + 1] = left[i] + right[i];
+    }
+    return res;
+  }
+  dfs(root);
+  return count;
+}
