64e7cf7621c6cddefd705dcf09318420f8f7145e
gerardoh13 <gerardoh13@gmail.com>
2024-08-07 01:38:15 -0700

    Solved DC 8/6/24

--- agusut24.js
+++ agusut24.js
@@ -85,6 +85,76 @@ function minimumPushes(word) {
 
 // 273. Integer to English Words
 
-function numberToWords(num){
-    
+function numberToWords(num) {
+  if (!num) return "Zero";
+  let str = num.toString();
+  let ans = [];
+  let count = 1;
+  let dict = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+    10: "Ten",
+    11: "Eleven",
+    12: "Twelve",
+    13: "Thirteen",
+    14: "Fourteen",
+    15: "Fifteen",
+    16: "Sixteen",
+    17: "Seventeen",
+    18: "Eighteen",
+    19: "Nineteen",
+    20: "Twenty",
+    30: "Thirty",
+    40: "Forty",
+    50: "Fifty",
+    60: "Sixty",
+    70: "Seventy",
+    80: "Eighty",
+    90: "Ninety",
+    100: "Hundred",
+    1000: "Thousand",
+    1000000: "Million",
+    1000000000: "Billion",
+  };
+  function helper(chunk) {
+    chunk = parseInt(chunk).toString()
+    let res = [];
+    if (!+chunk) return;
+    if (chunk.length === 3) {
+      res.push(dict[chunk[0]]);
+      res.push(dict[100]);
+      if (dict[chunk.slice(1)]) res.push(dict[chunk.slice(1)]);
+      else if (+chunk.slice(1)) {
+        if (+chunk[1]) res.push(dict[chunk[1] * 10]);
+        res.push(dict[chunk[2]]);
+      }
+    } else if (chunk.length === 2) {
+      if (dict[chunk]) res.push(dict[chunk]);
+      else {
+        res.push(dict[chunk[0] * 10]);
+        res.push(dict[chunk[1]]);
+      }
+    } else res.push(dict[chunk[0]]);
+    res = res.join(" ");
+    if (count > 1) res += " " + dict[count];
+    ans.unshift(res);
+  }
+  let chunk = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    chunk = str[i] + chunk;
+    if (chunk.length === 3 || i === 0) {
+      helper(chunk);
+      chunk = "";
+      count *= 1000;
+    }
+  }
+  return ans.join(" ");
 }
