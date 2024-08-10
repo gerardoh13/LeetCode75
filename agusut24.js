7987260bf28cdd3860393a7e85440843a85f434a
gerardoh13 <gerardoh13@gmail.com>
2024-08-09 23:57:23 -0700

    Started DC 8/9/24

--- agusut24.js
+++ agusut24.js
@@ -233,4 +233,57 @@ function numMagicSquaresInside(grid) {
   return res;
 }
 
+// 959. Regions Cut By Slashes
 
+class DS {
+  constructor(n) {
+    var root = [...new Array(n).keys()];
+    var rank = new Array(n).fill(0);
+    find = (v) => {
+      if (root[v] !== v) root[v] = this.find(root[v]);
+      return root[v];
+    };
+    union = (i, j) => {
+      var [ri, rj] = [this.find(i), this.find(j)];
+      if (ri === rj) return;
+      if (rank[ri] > rank[rj]) root[rj] = ri;
+      else if (rank[ri] < rank[rj]) root[ri] = rj;
+      else (root[ri] = rj), rank[rj]++;
+    };
+    getUnoinCount = () => {
+      for (var i = 0; i < n; i++) this.find(i);
+      return new Set(root).size;
+    };
+  }
+}
+
+function getKeys(i, j, n) {
+  var val = i * n + j;
+  val *= 2;
+  return [val, val + 1];
+}
+
+function regionsBySlashes(grid) {
+  var n = grid.length;
+  if (n === 1) return grid[0][0] === " " ? 1 : 2;
+  var ds = new DS(n * n * 2);
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      var [left, right] = getKeys(i, j, n);
+      if (grid[i][j] === " ") ds.union(left, right);
+      if (i !== 0) {
+        var [upLeft, upRight] = getKeys(i - 1, j, n);
+        var upKey = grid[i - 1][j] === "\\" ? upLeft : upRight;
+        var curKey = grid[i][j] === "/" ? left : right;
+        ds.union(upKey, curKey);
+      }
+      if (j !== 0) {
+        var [leftLeft, leftRight] = getKeys(i, j - 1, n);
+        var leftKey = leftRight;
+        var curKey = left;
+        ds.union(leftKey, curKey);
+      }
+    }
+  }
+  return ds.getUnoinCount();
+}
