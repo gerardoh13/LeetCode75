9c0b989326bf30d7e273711578324c165a186672
gerardoh13 <gerardoh13@gmail.com>
2024-09-18 00:27:53 -0700

    Solved DC 9/18/24

--- september24.js
+++ september24.js
@@ -352,3 +352,12 @@ function uncommonFromSentences(s1, s2) {
   }
   return Array.from(goodSet);
 }
+
+// 179. Largest Number
+
+function largestNumber(nums) {
+  let array = nums.map(String);
+  array.sort((a, b) => (b + a).localeCompare(a + b));
+  if (array[0] == "0") return "0";
+  return array.join("");
+}
