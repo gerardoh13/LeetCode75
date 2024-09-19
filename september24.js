4619903099138858fc082e0dc6735a9007a37bc9
gerardoh13 <gerardoh13@gmail.com>
2024-09-18 23:54:21 -0700

    Solved DC 9/19/24

--- september24.js
+++ september24.js
@@ -361,3 +361,25 @@ function largestNumber(nums) {
   if (array[0] == "0") return "0";
   return array.join("");
 }
+
+// 241. Different Ways to Add Parentheses
+
+function diffWaysToCompute(expression) {
+  let res = [];
+  for (let i = 0; i < expression.length; i++) {
+    let oper = expression[i];
+    if (oper === "+" || oper === "-" || oper === "*") {
+      let s1 = diffWaysToCompute(expression.slice(0, i));
+      let s2 = diffWaysToCompute(expression.slice(i + 1));
+      for (let a of s1) {
+        for (let b of s2) {
+          if (oper === "+") res.push(a + b);
+          else if (oper === "-") res.push(a - b);
+          else if (oper === "*") res.push(a * b);
+        }
+      }
+    }
+  }
+  if (res.length === 0) res.push(parseInt(expression));
+  return res;
+}
